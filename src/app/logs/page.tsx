971474fad302685ffdbf97af3a5aae66de3aae6c
Jason <79180247+Bobsters6666@users.noreply.github.com>
2024-01-16 15:58:08 +1300

    v1.02 log

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -7,6 +7,12 @@ const page = () => {
     <div className="max-w-[1080px] mx-auto mt-8">
       <h2 className="text-3xl font-bold underline">Logs</h2>
       <div>
+        <h3 className="text-xl font-bold mt-12 mb-4">v1.02</h3>
+        <ul className="flex flex-col gap-2">
+          <li className="">Added bulk upgrades for hero</li>
+        </ul>
+      </div>
+      <div>
         <h3 className="text-xl font-bold mt-12 mb-4">v1.01</h3>
         <ul className="flex flex-col gap-2">
           <li className="">Can now equip different equipments</li>
@@ -15,7 +21,8 @@ const page = () => {
           <li>Added shiny enemies</li>
         </ul>
       </div>
-      <div>
+
+      <div className="mt-8">
         <Button variant={"link"}>
           <Link href="/">Back</Link>
         </Button>
